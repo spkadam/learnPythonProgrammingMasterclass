18a67586d10d9cf340981667a3f44ae5d527b36e
skadam <skadam@danfoss.com>
2019-09-26 13:46:08 -0500

    started 09/26

---
+++ section5_Introduction/helloWorld.py
@@ -0,0 +1,27 @@
+
+###Lesson 3: getting to know python###
+
+# print('Hello World!')
+# print(1 + 2)
+# print(7*6)
+# print
+# print('"Python strings are easy to use"')
+
+# greeting = "Hola"
+# name = "Bruce"
+
+# print (greeting + " " + name)
+
+##################################################
+###Lesson 4: understanding more about python
+
+greeting = "Hello"
+name = input("Please enter your name: ")
+
+print(greeting + " "+ name)
+
+splitString = "This string has been \nsplit over \nseveral \nlines!"
+print(splitString)
+
+tabbedString = "1\t2\t3\t4\t5\t6"
+print(tabbedString)
